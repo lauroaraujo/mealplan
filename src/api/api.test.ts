6ca7bed9cde4eaf0d6a88607348ef1fccef0c38d
Lauro Araujo <lauro.araujo@gmail.com>
2020-01-12 13:14:44 -0500

    Console Infrastructure

--- src/api/api.test.ts
+++ src/api/api.test.ts
@@ -1,15 +1,23 @@
 import FoodbookAPI from './api'
+import Console from './infra/console'
 
 describe('Foodbook API', () => {
-  it('starts and stops correctly.', done => {
-    expect.assertions(1)
+  it('starts and stops correctly.', async (done) => {
+    const port = 33591
+    const console = Console.createNull(2)
 
-    expect(async () => {
-      const api = new FoodbookAPI()
-      await api.start()
-      await api.stop()
+    const api = new FoodbookAPI(port, console)
+    expect(api.getPort()).toBe(port)
 
-      done()
-    }).not.toThrow()
+    await api.start()
+    expect(console.getLastLog()).toBe(`Server listening on port ${port}`)
+
+    await api.stop()
+    expect(console.getHistory(2)).toEqual([
+      `Shutting down server on port ${port}...`,
+      `Server on port ${port} has stopped.`
+    ])
+
+    done()
   })
 })
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,21 +1,22 @@
 import { Server } from 'http'
 import express, { Express } from 'express'
+import Console from './infra/console'
 
 export default class FoodbookAPI {
   private app: Express
   private server: Server
 
-  constructor (private port: Number = 3500) {
+  constructor (private port: Number = 3500, private console = Console.create() ) {
     this.app = express()
     this.registerRoutes()
   }
 
   private onServerStarted = () => {
-    console.log(`Server listening on port ${this.port}`)
+    this.console.log(`Server listening on port ${this.port}`)
   }
 
   private onServerStopped = () => {
-    console.log(`Server on port ${this.port} has stopped.`)
+    this.console.log(`Server on port ${this.port} has stopped.`)
   }
 
   registerRoutes = () => {
@@ -26,7 +27,8 @@ export default class FoodbookAPI {
 
   stop = async () => {
     if (this.server) {
-      console.log(`Shutting down server on port ${this.port}...`)
+      this.console.log(`Shutting down server on port ${this.port}...`)
+
       return new Promise((resolve, reject) => {
         this.server.close((err) => {
           if (err) {
@@ -43,11 +45,15 @@ export default class FoodbookAPI {
   start = async () => {
     return new Promise((resolve, reject) => {
       try {
-        this.server = this.app.listen(this.port, this.onServerStarted)
-        resolve()
+        this.server = this.app.listen(this.port, () => {
+          this.onServerStarted()
+          resolve()
+        })
       } catch (err) {
         reject(err)
       }
     })
   }
+
+  getPort = () => this.port
 }
